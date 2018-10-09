5d05bec2eea105d8b43da0baac0ab002adc04908
saramanresa <sara.manresa@uvic.cat>
2018-10-09 17:17:50 +0200

    classify correctly any RNA or DNA sequence

--- seqClass.sh
+++ seqClass.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 seq=$1
 seq=$(echo $seq | tr a-z A-Z)  # Note we just added this line
-  if [[ $seq =~ T ]]; then
-    echo "The sequence is DNA"
-  elif [[ $seq =~ U ]]; then
-    echo "The sequence is RNA"
-  else
-    echo "The sequence can be DNA or RNA"
-  fi
+if [[ $seq =~ ^[ACGTU]+$ ]]; then
+  if [[ $seq =~ ^[ACGT]+$ ]]; then
+	echo "The sequence is DNA"
+  if [[ $seq =~ ^[ACGU]+$ ]]; then
+	echo "The sequence is RNA"
 else
   echo "The sequence is not DNA nor RNA"
 fi
 
-#edit
+#edit 
+	
+
+
